acdd36940a509ab64799c518ab6c497147001aea
Thorben Below <thorbenbelow@gmail.com>
2021-06-10 12:48:20 +0200

    add book/variables

---
+++ book/variables/src/main.rs
@@ -0,0 +1,33 @@
+fn main() {
+  let x = 5;
+
+  let x = x + 1;
+
+  let x = x * 2;
+
+  println!("The value of x is: {}", x);
+
+  let sum = 5 + 10;
+
+  // subtraction
+  let difference = 95.5 - 4.3;
+
+  // multiplication
+  let product = 4 * 30;
+
+  // division
+  let quotient = 56.7 / 32.2;
+
+  // remainder
+  let remainder = 43 % 5;
+
+  let tup = (500, 6.4, 1);
+
+  let (x, y, z) = tup;
+
+  println!("The value of y is: {}", y);
+
+  let a = [1, 2, 3, 4, 5];
+  let first = a[0];
+  let second = a[1];
+}
