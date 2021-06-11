168e605df376fc478c8b7b419df2bf961bae01d2
Thorben Below <thorbenbelow@gmail.com>
2021-06-11 20:13:49 +0200

    add book/structs

---
+++ book/structs/src/main.rs
@@ -0,0 +1,31 @@
+fn main() {
+  let mut user1 = User {
+    email: String::from("someone@example.com"),
+    username: String::from("someusername123"),
+    active: true,
+    sign_in_count: 1,
+  };
+  user1.email = String::from("anotheremail@example.com");
+  let user2 = build_user(String::from("m@m.m"), String::from("m"));
+  println!("{}, {}", user1.email, user2.username);
+  struct Color(i32, i32, i32);
+  struct Point(i32, i32, i32);
+  let black = Color(0, 0, 0);
+  let origin = Point(0, 0, 0);
+}
+
+fn build_user(email: String, username: String) -> User {
+  User {
+    email,
+    username,
+    active: true,
+    sign_in_count: 1,
+  }
+}
+
+struct User {
+  username: String,
+  email: String,
+  sign_in_count: u64,
+  active: bool,
+}
