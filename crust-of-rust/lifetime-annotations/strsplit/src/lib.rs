86cb53229af5f96cca6f0ea0e48b50178bcf1ddf
Thorben Below <thorbenbelow@gmail.com>
2022-11-10 21:47:32 +0100

    add lifetime basics

---
+++ crust-of-rust/lifetime-annotations/strsplit/src/lib.rs
@@ -0,0 +1,44 @@
+#[derive(PartialEq, Debug)]
+pub struct StrSplit<'a> {
+  remainder: &'a str,
+  delimiter: &'a str,
+}
+
+impl<'a> StrSplit<'a> {
+  pub fn new(haystack: &'a str, delimiter: &'a str) -> Self {
+    Self {
+      remainder: haystack,
+      delimiter,
+    }
+  }
+}
+
+impl<'a> Iterator for StrSplit<'a> {
+  type Item = &'a str;
+  fn next(&mut self) -> Option<Self::Item> {
+    if let Some(next_delim) = self.remainder.find(self.delimiter) {
+      let until_delimiter = &self.remainder[..next_delim];
+      self.remainder = &self.remainder[(next_delim + self.delimiter.len())..];
+      Some(until_delimiter)
+    } else if self.remainder.is_empty() {
+      None
+    } else {
+      let rest = self.remainder;
+      self.remainder = "";
+      Some(rest)
+    }
+    // find next pos in remainder
+    // return split until this pos
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  #[test]
+  fn it_works() {
+    let haystack = "a b c d e";
+    let letters = StrSplit::new(haystack, " ");
+    assert!(letters.eq(vec!["a", "b", "c", "d", "e"].into_iter()));
+  }
+}
