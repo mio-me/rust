e808cfaab8393aefec1341996a2c436b86424806
Thorben Below <thorbenbelow@gmail.com>
2022-11-19 16:46:26 +0100

    init channel fn

---
+++ crust-of-rust/channels/src/lib.rs
@@ -0,0 +1,73 @@
+use std::{
+  collections::VecDeque,
+  sync::{Arc, Condvar, Mutex},
+};
+
+pub struct Sender<T> {
+  inner: Arc<Inner<T>>,
+}
+
+impl<T> Clone for Sender<T> {
+  fn clone(&self) -> Self {
+    Sender {
+      inner: Arc::clone(&self.inner),
+    }
+  }
+}
+
+impl<T> Sender<T> {
+  pub fn send(&mut self, t: T) {
+    let mut queue = self.inner.queue.lock().unwrap();
+    queue.push_back(t);
+    drop(queue);
+    self.inner.available.notify_one();
+  }
+}
+
+pub struct Receiver<T> {
+  inner: Arc<Inner<T>>,
+}
+
+impl<T> Receiver<T> {
+  pub fn recv(&mut self) -> T {
+    let mut queue = self.inner.queue.lock().unwrap();
+    loop {
+      match queue.pop_front() {
+        Some(t) => return t,
+        None => {
+          queue = self.inner.available.wait(queue).unwrap();
+        }
+      }
+    }
+  }
+}
+
+struct Inner<T> {
+  queue: Mutex<VecDeque<T>>,
+  available: Condvar,
+}
+
+pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
+  let inner = Inner {
+    queue: Mutex::default(),
+    available: Condvar::new(),
+  };
+  let inner = Arc::new(inner);
+  (
+    Sender {
+      inner: inner.clone(),
+    },
+    Receiver {
+      inner: inner.clone(),
+    },
+  )
+}
+
+#[cfg(test)]
+mod tests {
+  #[test]
+  fn it_works() {
+    let result = 2 + 2;
+    assert_eq!(result, 4);
+  }
+}
